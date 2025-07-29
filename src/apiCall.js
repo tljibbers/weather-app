cc4b33540f2e0031e5d04e8d7416c1788ebf6eda
tljibbers <aahijoh@gmail.com>
2025-07-28 18:16:21 -0700

    added displays for current day and other days

--- src/apiCall.js
+++ src/apiCall.js
@@ -23,13 +23,88 @@ export async function recieveAPI()
 {
     const data = await callApi()
     console.log(data)
-    test(data)
+    displayCityAndTemp(data)
+    displayInformationHours(data)
 }
 
-function test(data)
+function displayCityAndTemp(data)
 {
+    const cityTempContainer = document.createElement("div")
+
+    const cityName = document.createElement("div")
+    cityName.innerHTML = data.resolvedAddress
+
+    const temp = document.createElement("div")
+    temp.innerHTML = String(Math.round(data.currentConditions.temp))
+
+    const condition = document.createElement("div")
+    condition.innerHTML = data.currentConditions.conditions
+
+    const highAndLowTemp = document.createElement("div")
+    highAndLowTemp.innerHTML = 'H:' + data.days[0].tempmax + ' ' + 'L:' + data.days[0].tempmin
+    
+    cityTempContainer.appendChild(cityName)
+    cityTempContainer.appendChild(temp)
+    cityTempContainer.appendChild(condition)
+    cityTempContainer.appendChild(highAndLowTemp)
+
+}
+function displayInformationHours(data)
+{
+    const descriptionContainer = document.createElement("div")
+    const description = document.createElement("div")
+    description.innerHTML = data.description
+    
+    descriptionContainer.appendChild(description)
+
     for(let i = 0; i < data.days[0].hours.length; i++)
     {
-        console.log(data.days[0].hours[i])
+        //console.log(data.days[0].hours[i])
+        const hourContainer = document.createElement("div")
+        const time = document.createElement("div")
+        time.innerHTML = data.days[0].hours[i].datetime
+        const weatherIcon = document.createElement("img")
+
+        const temperature = document.createElement("div")
+        temperature.innerHTML = String(Math.round(data.days[0].hours[i].temp))
+        console.log(temperature)
+
+        hourContainer.appendChild(time)
+        hourContainer.appendChild(weatherIcon)
+        hourContainer.appendChild(temperature)
+
+        descriptionContainer.appendChild(hourContainer)
+        
+
+    }
+}
+
+function determineIcon(image, data)
+{
+    if(data.currentConditions.conditions == "Clear")
+    {
+
+    }
+}
+
+function displayOtherDays(data)
+{
+    const fullWeekContainer = document.createElement("div")
+    const dayContainer = document.createElement("div")
+    for(let i = 1; i < data.days.length; i++)
+    {
+        const date = document.createElement("div")
+        date.innerHTML = data.days[i].datetime
+
+        const weatherIcon = document.createElement("img")
+
+        const temperature = document.createElement("div")
+        temperature.innerHTML = data.days[i].temp
+
+        dayContainer.appendChild(date)
+        dayContainer.appendChild(weatherIcon)
+        dayContainer.appendChild(temperature)
+
+        fullWeekContainer.appendChild(dayContainer)
     }
 }
